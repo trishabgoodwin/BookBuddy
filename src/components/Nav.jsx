6b612bb78a11dfba5fe728c99cd1328f1606c9a9
Trisha Goodwin <trishabromero@gmail.com>
2025-04-24 21:18:59 -0500

    unauthorized user components minus search

---
+++ src/components/Nav.jsx
@@ -0,0 +1,11 @@
+import { Routes, Route, Link } from "react-router-dom";
+
+function Nav() {
+    return (
+      <div id="navbar">
+          <Link to="/">Home</Link>
+      </div>
+    );
+  }
+  
+  export default Nav;
