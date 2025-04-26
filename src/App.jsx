a9342a2c5267d41e84f843a9ee71eee5ae1539c8
Trisha Goodwin <trishabromero@gmail.com>
2025-04-26 16:00:36 -0500

    user registration page successfully produces token and registers new user

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,12 @@ import {Route, Routes} from 'react-router-dom'
 import BookList from './components/BookList'
 import SingleBook from './components/SingleBook'
 import Nav from './components/Nav'
+import Register from './components/Register'
 
 function App() {
   const [books, setBooks] = useState([])
   const [book, setBook] = useState({})
+  const [token, setToken] = useState(localStorage.getItem("token") || null)
 
   return (
     <>
@@ -18,6 +20,7 @@ function App() {
     <Routes>
       <Route path="/" element={<BookList books={books} setBooks={setBooks} />} />
       <Route path="/SingleBook/:id" element={<SingleBook  book={book} setBook={setBook} books={books} setBooks={setBooks}/> } />
+      <Route path="/Register" element={<Register token={token} setToken={setToken} />}/>
     </Routes>
     </div>
     </>
